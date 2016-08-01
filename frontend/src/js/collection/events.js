9cc1c4a0d8881fc56ef8446723f0773b1c987fe4
ashrey <alberroteran@gmail.com>
2016-08-01 10:04:23 -0400

    Add event

--- frontend/src/js/collection/events.js
+++ frontend/src/js/collection/events.js
@@ -1,6 +1,10 @@
 var Evento     = require('model/event');
 var Backbone   = require('backbone');
+var _          = require('underscore');
 module.exports = Backbone.Collection.extend({
     model: Evento,
-    url: '/events'
+    url: '/events',
+    comparator: function(model) {
+        return model.get('title');
+    }
 });
